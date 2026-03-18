821a62907010220c5393018870840c7bfa614488
RamOfFate <ramoffate@gmail.com>
2026-03-18 21:18:46 +0100

    POST and GET (list) /api/records

--- sentinel/types/next-auth.d.ts
+++ sentinel/types/next-auth.d.ts
@@ -3,17 +3,20 @@ import NextAuth, { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       salt?: string;
     } & DefaultSession["user"];
   }
 
   interface User {
+    id: string;
     salt?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id: string;
     salt?: string;
   }
 }
